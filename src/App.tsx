9d704955b86455bce016b69932a7bc4471363bef
okamura123 <119551754+okamura123@users.noreply.github.com>
2023-01-31 18:03:04 +0900

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import "./styles.css";
+import axios from "axios";
+
+export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+    </div>
+  );
+}
+
 import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
