1f1d23b7d141d6ea48f6f285d976727f86efa75a
okamura123 <119551754+okamura123@users.noreply.github.com>
2023-02-01 15:49:24 +0900

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import "./styles.css";
 import axios from "axios";
-
-export default function App() {
-  const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
-  };
-  return (
-    <div className="App">
-      <button onClick={onClickFetchData}>データ取得</button>
-    </div>
-  );
-}
-
-import axios from "axios";
 import { useState } from "react";
-import "./styles.css";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user = (User) => {
+  name: "zz";
+  hobbies: ["映画", "ゲーム"];
+};
 
 export default function App() {
-  const [todos, settodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      settodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
